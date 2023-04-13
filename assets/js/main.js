b05668afefd6e4662356b0e2620264f7dcaf4280
Darneisha Miller <darneisham20@gmail.com>
2023-04-12 20:11:18 -0400

    Scroll Up Button / Home Image Sizing Started

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 /* ==== Menu Show and Hidden ==== */
+
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
 /* ==== MENU SHOW ==== */
+
 if(navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
@@ -11,6 +13,7 @@ if(navToggle) {
 }
 
 /* ==== MENU HIDDEN ==== */
+
 if(navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
@@ -18,6 +21,7 @@ if(navClose) {
 }
 
 /* ==== REMOVE MOBILE MENU ==== */
+
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () => {
@@ -61,4 +65,35 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     },
 });
 
-/* ==== TESTIMONIAL SWIPER ==== */
+/* ==== SCROLLING ACTIVE LINK ==== */
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                sectionsClass.classList.add('active-link')
+            } else {
+                sectionsClass.classList.remove('active-link')
+            }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/* ==== SHOW SCROLL UP BUTTON ==== */
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
